bed356858c1d6a35a07b3f0b39d863d3f66144a2
Danylo Zaitsev <danilverehko@gmail.com>
2025-03-31 14:30:31 +0200

    sdfsdf

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,11 @@
 function formatMessage(message, maxLength) {
-    textLenght = message.length;
-    if (textLenght <= maxLength) {
-      text = message;
-    } else {
-      text = message.slice(0, maxLength) + "...";
-    }
-    return text;
+  if (message.length <= maxLength) {
+      return message;
   }
-
-console.log(formatMessage('Curabitur ligula sapien', 16));
-console.log(formatMessage('Curabitur ligula sapien', 23));
-console.log(formatMessage('Vestibulum facilisis purus nec', 20));
-console.log(formatMessage('Vestibulum facilisis purus nec', 30));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
+  return message.slice(0, maxLength) + "...";
+}console.log(formatMessage("Curabitur ligula sapien", 16)); 
+console.log(formatMessage("Curabitur ligula sapien", 23)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
--- js/task-4.js
+++ js/task-4.js
@@ -1,26 +1,21 @@
 function getShippingCost(country) {
-    switch (country) {
+  switch (country) {
       case "China":
-        result = "Shipping to China will cost 100 credits";
-        break;
+          return "Shipping to China will cost 100 credits";
       case "Chile":
-        result = "Shipping to Chile will cost 250 credits";
-        break;
+          return "Shipping to Chile will cost 250 credits";
       case "Australia":
-        result = "Shipping to Australia will cost 170 credits";
-        break;
+          return "Shipping to Australia will cost 170 credits";
       case "Jamaica":
-        result = "Shipping to Jamaica will cost 120 credits";
-        break;
+          return "Shipping to Jamaica will cost 120> credits";
       default:
-        result = "Sorry, there is no delivery to your country";
-    }
-    return result;
+          return "Sorry, there is no delivery to your country";
   }
+}
 
-console.log(getShippingCost('Australia'));
-console.log(getShippingCost('Germany'));
-console.log(getShippingCost('China'));
-console.log(getShippingCost('Chile'));
-console.log(getShippingCost('Jamaica'));
-console.log(getShippingCost('Sweden'));
+console.log(getShippingCost("Australia")); 
+console.log(getShippingCost("Germany")); 
+console.log(getShippingCost("China")); 
+console.log(getShippingCost("Chile")); 
+console.log(getShippingCost("Jamaica")); 
+console.log(getShippingCost("Sweden"));
